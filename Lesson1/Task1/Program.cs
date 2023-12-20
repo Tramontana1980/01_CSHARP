f8e82341706c146d5da569a9e49248f0044fb6d5
Alexey Filippchik <fila1980@mail.ru>
2023-12-20 23:56:55 +0300

    add some variables lesson1:task1

--- Lesson1/Task1/Program.cs
+++ Lesson1/Task1/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello wrold");
+﻿//Console.WriteLine("Hello world");
+
+
+int value = 100;
+Console.WriteLine(value);
+
+double d = 5.55;
+char c = 'L';
+bool b = true;
+Console.WriteLine(d);
+Console.WriteLine(c);
+Console.WriteLine(b);
